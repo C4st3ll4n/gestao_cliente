3e681a9b3f776df12e1d5550f3f854782d1db9de
Pedro Henrique <p13dr0h@gmail.com>
2019-04-18 05:39:24 -0300

    Ajustes

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'gestaozinea.herokuapp.com/'
+]
 
 
 # Application definition
